56c382038b70d0b9b73818a95e928f5d97ae2a99
Peter Svensson <peter@sparetimecoders.com>
2024-02-02 14:09:29 +0100

    chore: wip

---
+++ handler.go
@@ -0,0 +1,162 @@
+// MIT License
+//
+// Copyright (c) 2024 sparetimecoders
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package goamqp
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+type Handler func(ctx context.Context, event any) (any, error)
+
+// EventHandler is the type definition for a function that is used to handle events of a specific type.
+// TODO HandlerFunc is used to process an incoming message
+// If processing fails, an error should be returned and the message will be re-queued
+// The optional response is used automatically when setting up a RequestResponseHandler, otherwise ignored
+
+type EventHandler[T any] func(ctx context.Context, event ConsumableEvent[T]) (any, error)
+
+// Handlers holds the handlers for a certain queue
+type Handlers map[string]wrappedHandler
+
+// Get returns the handler for the given queue and routing key that matches
+func (h *Handlers) Get(routingKey string) (wrappedHandler, bool) {
+	for mappedRoutingKey, handler := range *h {
+		if match(mappedRoutingKey, routingKey) {
+			return handler, true
+		}
+	}
+	return nil, false
+}
+
+// exists returns the already mapped routing key if it exists (matched by the matches function to support wildcards)
+func (h *Handlers) exists(routingKey string) (string, bool) {
+	for mappedRoutingKey := range *h {
+		if overlaps(routingKey, mappedRoutingKey) {
+			return mappedRoutingKey, true
+		}
+	}
+	return "", false
+}
+
+func (h *Handlers) add(routingKey string, handler wrappedHandler) {
+	(*h)[routingKey] = handler
+}
+
+// QueueHandlers holds all handlers for all queues
+type QueueHandlers map[string]*Handlers
+
+// Add a handler for the given queue and routing key
+func (h *QueueHandlers) Add(queueName, routingKey string, handler wrappedHandler) error {
+	queueHandlers, ok := (*h)[queueName]
+	if !ok {
+		queueHandlers = &Handlers{}
+		(*h)[queueName] = queueHandlers
+	}
+
+	if mappedRoutingKey, exists := queueHandlers.exists(routingKey); exists {
+		return fmt.Errorf("routingkey %s overlaps %s for queue %s, consider using AddQueueNameSuffix", routingKey, mappedRoutingKey, queueName)
+	}
+	queueHandlers.add(routingKey, handler)
+	return nil
+}
+
+type Queue struct {
+	Name     string
+	Handlers *Handlers
+}
+
+// Queues returns all queue names for which we have added a handler
+func (h *QueueHandlers) Queues() []Queue {
+	if h == nil {
+		return []Queue{}
+	}
+	var res []Queue
+	for q, h := range *h {
+		res = append(res, Queue{Name: q, Handlers: h})
+	}
+	return res
+}
+
+// Handlers returns all the handlers for a given queue, keyed by the routing key
+func (h *QueueHandlers) Handlers(queueName string) *Handlers {
+	if h == nil {
+		return &Handlers{}
+	}
+
+	if handlers, ok := (*h)[queueName]; ok {
+		return handlers
+	}
+	return &Handlers{}
+}
+
+// wrappedHandler is internally used to wrap the generic EventHandler
+// this is to facilitate adding all the different type of T on the same map
+type wrappedHandler func(ctx context.Context, event unmarshalEvent) (any, error)
+
+func newWrappedHandler[T any](handler EventHandler[T]) wrappedHandler {
+	return func(ctx context.Context, event unmarshalEvent) (any, error) {
+		consumableEvent := ConsumableEvent[T]{
+			Metadata:     event.Metadata,
+			DeliveryInfo: event.DeliveryInfo,
+		}
+		err := json.Unmarshal(event.Payload, &consumableEvent.Payload)
+		if err != nil {
+			return nil, err
+		}
+		return handler(ctx, consumableEvent)
+	}
+}
+
+// overlaps checks if two AMQP binding patterns overlap
+func overlaps(p1, p2 string) bool {
+	if p1 == p2 {
+		return true
+	} else if match(p1, p2) {
+		return true
+	} else if match(p2, p1) {
+		return true
+	}
+	return false
+}
+
+// match returns true if the AMQP binding pattern is matching the routing key
+func match(pattern string, routingKey string) bool {
+	b, err := regexp.MatchString(fixRegex(pattern), routingKey)
+	if err != nil {
+		return false
+	}
+	return b
+}
+
+// fixRegex converts the AMQP binding key syntax to regular expression
+// For example:
+// user.* => user\.[^.]*
+// user.# => user\..*
+func fixRegex(s string) string {
+	replace := strings.Replace(strings.Replace(strings.Replace(s, ".", "\\.", -1), "*", "[^.]*", -1), "#", ".*", -1)
+	return fmt.Sprintf("^%s$", replace)
+}
--- tracing.go
+++ tracing.go
@@ -1,6 +1,6 @@
 // MIT License
 //
-// Copyright (c) 2019 sparetimecoders
+// Copyright (c) 2024 sparetimecoders
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -22,8 +22,35 @@
 
 package goamqp
 
-// errorLogf function called for error logs
-type errorLog func(s string)
+import (
+	"context"
 
-// noOpLogger log function that does nothing
-var noOpLogger = func(s string) {}
+	amqp "github.com/rabbitmq/amqp091-go"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+)
+
+// inject the span context to amqp table
+func injectToHeaders(ctx context.Context) amqp.Table {
+	carrier := propagation.MapCarrier{}
+	otel.GetTextMapPropagator().Inject(ctx, carrier)
+
+	header := amqp.Table{}
+	for k, v := range carrier {
+		header[k] = v
+	}
+	return header
+}
+
+// extract the amqp table to a span context
+func extractToContext(headers amqp.Table) context.Context {
+	carrier := propagation.MapCarrier{}
+	for k, v := range headers {
+		value, ok := v.(string)
+		if ok {
+			carrier[k] = value
+		}
+	}
+
+	return otel.GetTextMapPropagator().Extract(context.TODO(), carrier)
+}
