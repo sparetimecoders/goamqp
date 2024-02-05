d1f7f55b68802ea51dd1dc6baad46649de18190b
Peter Svensson <peter@sparetimecoders.com>
2024-02-05 23:53:18 +0100

    chore: wip

--- request_response_test.go
+++ request_response_test.go
@@ -1,3 +1,25 @@
+// MIT License
+//
+// Copyright (c) 2024 sparetimecoders
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package goamqp
 
 import (
@@ -28,8 +50,8 @@ func Test_RequestResponseHandler(t *testing.T) {
 	require.Equal(t, 1, len(channel.BindingDeclarations))
 	require.Equal(t, BindingDeclaration{queue: "svc.direct.exchange.request.queue", noWait: false, exchange: "svc.direct.exchange.request", key: "key", args: amqp.Table{}}, channel.BindingDeclarations[0])
 
-	require.Equal(t, 1, len(conn.queueHandlers.Queues()))
+	require.Equal(t, 1, len(conn.queueHandlers.queues()))
 
-	handler, _ := conn.queueHandlers.Handlers("svc.direct.exchange.request.queue").get("key")
+	handler, _ := conn.queueHandlers.handlers("svc.direct.exchange.request.queue").get("key")
 	require.Equal(t, "github.com/sparetimecoders/goamqp.ServiceRequestConsumer[...].func1", runtime.FuncForPC(reflect.ValueOf(handler).Pointer()).Name())
 }
